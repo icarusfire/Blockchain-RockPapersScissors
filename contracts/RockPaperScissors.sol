c544916160e2e53ab11ba5e51833d79fafc51586
icarus fire <icaruss@icaruss-MacBook-Pro-2.local>
2020-04-20 17:29:58 +0200

    simplify code

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -5,158 +5,50 @@ import "./SafeMath.sol";
 import "./HitchensUnorderedAddressSet.sol";
 
 contract RockPaperScissors is Pausable {
-    using SafeMath for uint256;
-    using HitchensUnorderedAddressSetLib for HitchensUnorderedAddressSetLib.Set;
-    HitchensUnorderedAddressSetLib.Set openGameRequests;
-    uint public constant expiryDuration = 1 hours;
 
-    event FundsTransferedToOwnerEvent(address indexed owner, uint256 amount);
-    event LogWithdrawEvent(address indexed sender, uint256 amountDrawn);
-    constructor(bool _pausable) Pausable(_pausable) public {}
-
+    mapping(address => uint256) public players;
+    mapping(address => Game) public games;
     enum Move{NoMove, Rock, Paper, Scissors}
 
-    mapping(bytes32 => address) public commits;
-    mapping(address => uint256) public players;
-
-    mapping(address => Game) public games;
-
     struct Game {
-        address player2;
-        uint256 amount;
-        bool isStarted;
-        bool isAccepted;
-        uint gameStartedBlockNumber;
-        address winner;
-
-        Move movePlayer1;
-        Move movePlayer2;
+            uint256 amount;   //zero amount means game ended
+            Move movePlayer2; //means game started
+            address player2; //means game accepted
+            uint gameStartedBlockNumber;
+            bytes32 moveHash; //
     }
 
-
-/*  FLOW
-    - Player1 (game creator) creates a game by sending some gas(the money he wants to bet)
-    - Player2 wanst to join a game, he can get all open games and their money requirement.
-    - Player2 calls method requestToJoinGame().
-    - Player1 can check if anyone wants to join by polling checkCandidateOpponent method. He can decline a request or accept a potential candidate.
-    - Player2 polls checkIfGameIsAccepted() to see if his join request is accepted. If accepted he calls joinToAcceptedGame() Sends a calculated hash of his move: keccak( salt + move)
-    - Both players poll if game.isStarted = true; If so they call method reveal(Move move, bytes32 salt). Yhis method checks if hash can be calculated correctly, and if so whose move wins.
-
-
-To be fixed:
-- What is the best time Player1 sends his move? If he sends in openNewGame() Then he has to choose his move without knowing his opponent,
- this can be a slight disadvantage because other player knows his opponent before making his move.
-
-- Player1 should be able to blacklist people. 
-- Using address as key might not be the best option for Player1. Currenlty he cant restart a new game until he withdraws the money
-
-*/
-
-
-//*********************** Functions for the game creator ******************************************************** */
-
-    //Open a new game wait for an opponent
-    function openNewGame() public payable whenRunning{
+    function createNewGame(bytes32 moveHash) public payable whenRunning{
+        //save amountP1 and moveHash
         address key = msg.sender;
-        openGameRequests.insert(key); // Note that this will fail automatically if the key already exists.
         Game storage game = games[key];
         game.amount = msg.value;
-        game.isStarted = false;
+        game.moveHash = moveHash;
+        //add money to account
     }
 
-    // poll for candidates
-    function checkCandidateOpponent() public view returns (address){
-        Game storage game = games[msg.sender];
-        return game.player2;
-    }
-
-    //If after polling, there is a potential candidate, accept the challenge and make the move.
-    function acceptAndStartGame(bytes32 moveHash) public {
-        Game storage game = games[msg.sender];
-        game.isAccepted = true;
-        commits[moveHash] = msg.sender;
-    }
-
-    //If after polling, there is a nasty candidate, decline, (add him to blacklist.)
-    function declineTheRequest() public {
-        Game storage game = games[msg.sender];
-        game.isAccepted = false;
-        game.player2 = address(0);
-    }
-//********************************************************************************************************************** */
-
-
-//*********************** Functions for the candidate opponent ******************************************************** */
-
-    //Get available games
-    function getOpenGames() public whenRunning returns (bytes32[] memory) {
-           //iterate over openGameRequests
-           //return a list of addreses
-    }
-
-    //Request to join an open game someone created with their address
-    function requestToJoinGame(address addr) public payable whenRunning{
-        //check if msg.value is same as requested
-        //check game.player2 is empty
-        Game storage game = games[addr];
+    function joinGame(Move movePlayer2, address addressPlayer1) public payable whenRunning{
+        Game storage game = games[addressPlayer1];
+        require(msg.value == game.amount, "amount should be same");
+        game.movePlayer2 = movePlayer2;
         game.player2 = msg.sender;
     }
 
-    // poll if game is accepted
-    function checkIfGameIsAccepted(address addr) public view whenRunning returns (bool){
-        Game storage game = games[addr];
-        return game.isAccepted;
-    }
-
-    //After polling, when game is accepted, player2 joins the game by his moves hash, game is offically started.
-    function joinToAcceptedGame(bytes32 moveHash, address addressGameCreator) public whenRunning{
-        Game storage game = games[addressGameCreator];
-        require(game.player2 == msg.sender, "Challange should be accepted to join");
-        require(game.isAccepted, "Challange should be accepted to join");
-        require(moveHash > 0, "Hash can not be zero");
-        game.gameStartedBlockNumber = block.number;
-        commits[moveHash] = addressGameCreator;
-        game.isStarted = true;
-    }
-
-//********************************************************************************************************************** */
-
-    //after polling, if game is started reveal your hand
-    function reveal(Move move, bytes32 salt) public payable whenRunning returns (bool) {
+    function revealCalculate(bytes32 salt, Move movePlayer1) public whenRunning{
+        Game storage game = games[msg.sender];
+        require(game.player2 != address(0), "Game shoud be started");
         require(salt > 0, "salt should not be empty");
-        bytes32 moveHash = calculateMoveHash(move, salt);
-        address gameCreatorAddress = commits[moveHash];
-        require(gameCreatorAddress != address(0), "claimed hash should exist");
-        Game storage game = games[gameCreatorAddress];
-        require(game.gameStartedBlockNumber < block.number, "only reveal if commit is mined");
-        require(game.winner == address(0), "winner is not settled yet");
+        require(calculateMoveHash(movePlayer1, salt) == game.moveHash, "Hashes dont match");
+        //decideWinner(movePlain1, movePlain2)
+        //add money to winners account
 
-        if(game.player2 == msg.sender){
-            game.movePlayer2 = move;
-        }
-        else if(gameCreatorAddress == msg.sender){
-            game.movePlayer1 = move;
-        }
-        else {
-            revert("game should belong to the sender");
-        }
-
-        address winner = tryToSettle(game.movePlayer1, game.movePlayer2, gameCreatorAddress, game.player2);
-        if(winner != address(0)){
-            game.winner = winner;
-            //do accounting
-            //when to remove account?
-            openGameRequests.remove(gameCreatorAddress);
-            return true;
-        }
-        return false;
-    }
-
-    function tryToSettle(Move moverPlayer1, Move moverPlayer2, address player1, address player2) public view returns (address){
-        //compare enums(ROCK,PAPER,SCISSORS) of both players. decide winner.
-        //both should exist
-        //return address of winner
-        return address(0);
+        // Game storage game = games[addressGameCreator];
+        // require(game.player2 == msg.sender, "Challange should be accepted to join");
+        // require(game.isAccepted, "Challange should be accepted to join");
+        // require(moveHash > 0, "Hash can not be zero");
+        // game.gameStartedBlockNumber = block.number;
+        // commits[moveHash] = addressGameCreator;
+        // game.isStarted = true;
     }
 
     function calculateMoveHash(Move move, bytes32 salt) public pure returns (bytes32){
@@ -164,23 +56,7 @@ To be fixed:
         return keccak256(abi.encodePacked(move, salt));
     }
 
-    function withdraw(uint256 amount) public whenRunning {
-        require (amount > 0, "Withdraw amount should be higher than 0");
-        uint256 balanceSender = players[msg.sender];
-        players[msg.sender] = balanceSender.sub(amount);
-        emit LogWithdrawEvent(msg.sender, amount);
-        (bool success, ) = msg.sender.call.value(amount)("");
-        require(success, "Transfer failed.");
-    }
-
-    function transferFunds() public whenKilled onlyOwner {
-        uint256 amount = address(this).balance;
-        emit FundsTransferedToOwnerEvent(msg.sender, amount);
-        (bool success, ) = msg.sender.call.value(amount)("");
-        require(success, "Transfer failed.");
-    }
-
-    function isExpired(uint expiryTime) public view returns (bool) {
-        return now >= expiryTime;
+    function rejectPlayer() public whenRunning{
+        //set movePlayer2 and player2 null
     }
 }
